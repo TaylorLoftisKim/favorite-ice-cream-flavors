6de585ec183ac2778616b0db888790c3fec0ad9f
Cody Brubaker and Taylor Loftis-Kim <cody@>
2016-10-24 11:26:04 -0700

    show our favorite flavors with arrays

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  var flavors = ['vanilla', 'chocolate', 'strawberry'];
+  flavors.forEach(function(flavor) {
+
+  $(".flavor1").text(flavors[0]);
+  $(".flavor2").text(flavors[1]);
+  $(".flavor3").text(flavors[2]);
+
+
+  })
+})
